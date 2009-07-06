494efe1111317fcdb8a19c073c4eb81dc9ce1de8
Zachary Danger <zacharydangercampbell@gmail.com>
2009-07-06 15:43:00 -0500

    Adding a require for defines.php

--- loose_leaf.php
+++ loose_leaf.php
@@ -1,5 +1,6 @@
 <?php
 //getting allll loooooosey goooosey.
 set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__));
+require_once 'defines.php';
 require_once 'functions/functions.php';
 ?>
