350289a05b9f237d2d281faa8c5f5eb115f5f110
Zachary Danger <zacharydangercampbell@gmail.com>
2009-07-06 15:40:35 -0500

    Major file restructuring.

---
+++ functions/functions.php
@@ -0,0 +1,6 @@
+<?php
+/* Require some stuff. */
+require_once 'db.php';
+require_once 'html.php';
+require_once 'standard_lib.php';
+?>
---
+++ loose_leaf.php
@@ -0,0 +1,5 @@
+<?php
+//getting allll loooooosey goooosey.
+set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__));
+require_once 'functions/functions.php';
+?>
