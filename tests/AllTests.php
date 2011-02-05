c0bfd7eec3638f20039462e91a8d3cdb4187a637
Zachary Danger <zacharydangercampbell@gmail.com>
2011-02-05 13:39:44 -0600

    Fix some namespace collissions in a personal project

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -9,9 +9,9 @@ require_once __DIR__ . '/HtmlTemplateTest.php';
 
 class LooseLeaf_AllTests {
 	public static function suite() {
-		$suite = new PHPUnit_Framework_TestSuite('HabitMakr - AllTests');
+		$suite = new PHPUnit_Framework_TestSuite('LooseLeaf - AllTests');
 		$suite->addTestSuite('RouterTest');
-		$suite->addTestSuite('ControllerTest');
+		$suite->addTestSuite('\LooseLeaf\Tests\ControllerTest');
 		$suite->addTestSuite('DispatcherTest');
 		$suite->addTestSuite('RedirectorTest');
 		$suite->addTestSuite('ControllerLocatorTest');
--- tests/ControllerTest.php
+++ tests/ControllerTest.php
@@ -1,4 +1,6 @@
 <?php
+namespace LooseLeaf\Tests;
+
 require_once __DIR__ . '/helpers/Controller_TestSub.php';
 require_once __DIR__ . '/../Controller.php';
 
@@ -8,16 +10,16 @@ class ControllerTest extends \PHPUnit_Framework_TestCase {
 	 * @expectedException LooseLeaf\Redirect_Exception
 	 */
 	public function testRedirect_ThrowsException() {
-		$C = new Controller_TestSub();
+		$C = new \Controller_TestSub();
 		$C->subRedirect('/foobar.php');
 	}
 
 	public function testRedirect_ThrowsException_CheckLocation() {
-		$C = new Controller_TestSub();
+		$C = new \Controller_TestSub();
 		try {
 			$C->subRedirect('/foobar.php');
 			$this->fail("Should have thrown a Redirect_Exception.");
-		} catch(LooseLeaf\Redirect_Exception $e) {
+		} catch(\LooseLeaf\Redirect_Exception $e) {
 			$this->assertEquals('/foobar.php', $e->getLocation());
 		}
 	}
